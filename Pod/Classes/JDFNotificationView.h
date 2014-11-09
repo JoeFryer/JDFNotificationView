8b52c3410ad219959d0616764346011506ed44f4
Joe Fryer <joe.d.fryer@gmail.com>
2014-11-09 21:20:44 +0000

    Added JDFNotificationView.

---
+++ Pod/Classes/JDFNotificationView.h
@@ -0,0 +1,13 @@
+//
+//  JDFNotificationView.h
+//  JDFNotificationView
+//
+//  Created by Joe Fryer on 09/11/2014.
+//  Copyright (c) 2014 Joe Fryer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDFNotificationView : UIView
+
+@end
---
+++ Pod/Classes/JDFNotificationView.m
@@ -0,0 +1,21 @@
+//
+//  JDFNotificationView.m
+//  JDFNotificationView
+//
+//  Created by Joe Fryer on 09/11/2014.
+//  Copyright (c) 2014 Joe Fryer. All rights reserved.
+//
+
+#import "JDFNotificationView.h"
+
+@implementation JDFNotificationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
